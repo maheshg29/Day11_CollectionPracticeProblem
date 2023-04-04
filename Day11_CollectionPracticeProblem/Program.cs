0ddc149afb6ae4e37ef134bf72b0d3b626d920e2
mahesh <gangurdemahesh9@gmail.com>
2023-04-04 17:59:28 +0530

    [mahesh]add program file for welcome greeting message

---
+++ Day11_CollectionPracticeProblem/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11_CollectionPracticeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value");
+            int n = int.Parse(Console.ReadLine());
+            int[][] chapters = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                chapters[i] = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            }
+            int k = int.Parse(Console.ReadLine());
+
+            // Find the chapter Amelia is currently reading
+            int currentChapter = -1;
+            for (int i = 0; i < n; i++)
+            {
+                if (chapters[i][0] <= k && k <= chapters[i][1])
+                {
+                    currentChapter = i;
+                    break;
+                }
+            }
+
+            // Count the remaining chapters
+            int remainingChapters = n - currentChapter;
+            Console.WriteLine(remainingChapters);
+        }
+    }
+}
