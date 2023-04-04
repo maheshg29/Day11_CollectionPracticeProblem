0fdaefb22415ab4615e7a23c2dcbdcba57af12c3
mahesh <gangurdemahesh9@gmail.com>
2023-04-04 18:26:44 +0530

    [mahesh]add program file for find Remaining Chapters count

--- Day11_CollectionPracticeProblem/Program.cs
+++ Day11_CollectionPracticeProblem/Program.cs
@@ -11,7 +11,20 @@ namespace Day11_CollectionPracticeProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Day 11 collection practice Problem ");
-           
+            Console.WriteLine("Select any one option for Respective task\n" +
+                "1. Count Of Remaining Chapters");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    ChaptersCount chaptersCount = new ChaptersCount();
+                    chaptersCount.RemainingChapters();
+                    break;
+
+                default:
+                    break;
+            }
+
         }
     }
 }
