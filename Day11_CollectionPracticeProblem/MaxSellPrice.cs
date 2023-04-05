897449053bf1b000bf3da490c2f8f0f47d617fb7
mahesh <gangurdemahesh9@gmail.com>
2023-04-05 18:40:34 +0530

    [mahesh]add program file for find max selling price of basket

---
+++ Day11_CollectionPracticeProblem/MaxSellPrice.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11_CollectionPracticeProblem
+{ 
+    public class MaxSellPrice
+    {
+        public void MaxSell()
+        {
+            Console.WriteLine("Enter N and K value seprated by white space ");
+            var obj=Console.ReadLine().Split();
+            int n = Convert.ToInt32(obj[0]);
+            int k= Convert.ToInt32(obj[1]);
+
+            Console.WriteLine("Enter {0} value for basket A of fruit seprated by white space ", n);
+            string[] data = Console.ReadLine().Split();
+            int[] basket_A = data.Select(int.Parse).ToArray();
+
+            Console.WriteLine("Enter {0} value for basket B of fruit seprated by white space ", n);
+            data = Console.ReadLine().Split();
+            int[] basket_B = data.Select(int.Parse).ToArray();
+
+            //swap
+            for (int i = 0; i < k; i++)
+            {
+                int temp;
+                temp = basket_A[i];
+                basket_A[i] = basket_B[i];
+                basket_B[i] = temp;
+            }
+
+            Console.WriteLine("\nafter swaping basket A value is ");
+            foreach (int value in basket_A)
+            {
+                Console.Write(" " + value);
+            }
+
+            Console.WriteLine("\nafter swaping basket B value is ");
+            foreach (int value in basket_B)
+            {
+                Console.Write(" "+value);
+            }
+            int maxPriceInBaslet_A=0;
+            foreach (int value in basket_A)
+            {
+                maxPriceInBaslet_A +=value;
+            }
+            Console.WriteLine("\n ---------*****---------\n Max Selling Price of basket A is " + maxPriceInBaslet_A);
+
+        }
+    }
+}
--- Day11_CollectionPracticeProblem/Program.cs
+++ Day11_CollectionPracticeProblem/Program.cs
@@ -12,7 +12,8 @@ namespace Day11_CollectionPracticeProblem
         {
             Console.WriteLine("Welcome to Day 11 collection practice Problem ");
             Console.WriteLine("Select any one option for Respective task\n" +
-                "1. Count Of Remaining Chapters");
+                "1. Count Of Remaining Chapters\n" +
+                "2. Max Selling Price ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -21,7 +22,13 @@ namespace Day11_CollectionPracticeProblem
                     chaptersCount.RemainingChapters();
                     break;
 
+                case 2:
+                    MaxSellPrice maxSellPrice= new MaxSellPrice();
+                    maxSellPrice.MaxSell();
+                    break;
+
                 default:
+                    Console.WriteLine("Please Enter valid option");
                     break;
             }
 
